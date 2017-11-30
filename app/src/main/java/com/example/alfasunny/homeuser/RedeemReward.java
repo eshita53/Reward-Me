ba029e04858851c0541d7caed256b870e242caea
Tanmoytkd <tanmoykrishnadas@gmail.com>
2017-11-30 21:16:38 +0600

    added qr code scanner in redeem reward

--- app/src/main/java/com/example/alfasunny/homeuser/RedeemReward.java
+++ app/src/main/java/com/example/alfasunny/homeuser/RedeemReward.java
@@ -1,13 +1,118 @@
 package com.example.alfasunny.homeuser;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.alfasunny.homeuser.backend.DataHelper;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import static java.lang.Thread.sleep;
 
 public class RedeemReward extends AppCompatActivity {
-
+    DataHelper d;
+    TextView uidText;
+    ImageView qrImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_redeem_reward);
+
+        d = new DataHelper();
+
+        uidText = (TextView) findViewById(R.id.uidText);
+        uidText.setText(d.getUid());
+
+        qrImage = (ImageView) findViewById(R.id.qrImage);
+
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(d.getUid(), BarcodeFormat.QR_CODE, 200, 200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            qrImage.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //Common task for many activities
+
+
+        TextView name = (TextView) findViewById(R.id.displayName);
+        name.setText(d.getName());
+
+        new Thread(new Runnable() {
+            Integer earned;
+            Integer redeemed;
+            Integer totalpoints;
+            TextView earnedTxt;
+            TextView redeemTxt;
+            TextView totalPointsTxt;
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        earned = d.getTotalEarning();
+                        earnedTxt = (TextView) findViewById(R.id.totalEarned);
+
+                        redeemed = d.getTotalRedeem();
+                        redeemTxt = (TextView) findViewById(R.id.totalRedeemed);
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                earnedTxt.setText(getString(R.string.earned_points) + earned.toString());
+                                redeemTxt.setText(getString(R.string.redeemed_points) + redeemed.toString());
+                            }
+                        });
+
+
+                        sleep(1000);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+        findViewById(R.id.btnHome).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeIntent = new Intent(getBaseContext(), Home.class);
+                startActivity(homeIntent);
+            }
+        });
+
+        findViewById(R.id.btnNotification).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent notificationIntent = new Intent(getBaseContext(), Notifications.class);
+                startActivity(notificationIntent);
+            }
+        });
+
+        findViewById(R.id.btnProfile).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent profileIntent = new Intent(getBaseContext(), Profile.class);
+                startActivity(profileIntent);
+            }
+        });
+
+        findViewById(R.id.btnMore).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent moreIntent = new Intent(getBaseContext(), More.class);
+                startActivity(moreIntent);
+            }
+        });
     }
 }
